4b453ed93b9d904d6cb08253712e39c6a8f9629b
erickson-ureta <erick.ureta95@gmail.com>
2024-12-12 02:38:24 -0600

    Added base atom class, and implemented the ftyp atom

---
+++ src/atoms.cpp
@@ -0,0 +1,39 @@
+#include <string>
+
+#include "atoms.hpp"
+#include "buffer_utils.hpp"
+#include "logger.hpp"
+
+const std::string
+GenericAtom::getAtomName()
+{
+    return _mAtomName;
+}
+
+void
+FtypAtom::_parseRawBufIntoFields()
+{
+    uint8_t *cursor = _mRawBuffer;
+
+    _mMajorBrand = BufferUtils::read4BytesIntoU32(cursor);
+    cursor += 4;
+    _mMinorVersion = BufferUtils::read4BytesIntoU32(cursor);
+    cursor += 4;
+    while (cursor < _mRawBuffer + _mSize)
+    {
+        _mCompatibleBrands.push_back(BufferUtils::read4BytesIntoU32(cursor));
+        cursor += 4;
+    }
+}
+
+void
+FtypAtom::_debugPrint()
+{
+    Logger::get().info("major_brand = %u", static_cast<unsigned int>(_mMajorBrand));
+    Logger::get().info("minor_version = %u", static_cast<unsigned int>(_mMinorVersion));
+    Logger::get().info("compatible_brads = ");
+    for (const auto &i : _mCompatibleBrands)
+    {
+        Logger::get().info("%u", static_cast<unsigned int>(i));
+    }
+}
---
+++ src/atoms.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+#include "constants.hpp"
+
+class GenericAtom
+{
+    public:
+        GenericAtom(const std::string& atomName, size_t size, uint8_t *rawBuffer)
+            : _mAtomName(atomName), _mSize(size), _mRawBuffer(rawBuffer)
+        {
+            _parseRawBufIntoFields();
+        }
+
+        const std::string getAtomName();
+
+    protected:
+        // Common fields across all atoms
+        uint8_t version;
+        // TODO: figure out what to do with the "flags" field later
+
+        // Metadata stuff
+        const std::string _mAtomName;
+        size_t _mSize;
+        bool _mHasChildren;
+        size_t _mChildrenOffset;
+        uint8_t *_mRawBuffer;
+
+        virtual void _parseRawBufIntoFields();
+        virtual void _debugPrint();
+};
+
+class FtypAtom : public GenericAtom
+{
+    public:
+        FtypAtom(size_t size, uint8_t *rawBuffer)
+            : GenericAtom(ATOM_FTYP, size, rawBuffer) {}
+
+    protected:
+        uint32_t _mMajorBrand;
+        uint32_t _mMinorVersion;
+        std::vector<uint32_t> _mCompatibleBrands;
+
+        void _parseRawBufIntoFields() override;
+        void _debugPrint() override;
+};
+
