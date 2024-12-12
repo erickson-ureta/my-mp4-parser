0b18aed70df195a1710357dd232c6ebac07b0fc1
erickson-ureta <erick.ureta95@gmail.com>
2024-12-12 00:48:06 -0600

    Added a quick script to run through the whole CMake setup, compiling and running the program

---
+++ build_and_run.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+BUILDDIR=${PWD}/build
+
+print_err_and_exit() {
+    echo "${1}"
+    exit 1
+}
+
+if [ ! -d "${BUILDDIR}" ]; then
+    mkdir -p ${BUILDDIR} || print_err_and_exit "Unable to generate build directory"
+fi
+
+pushd ${BUILDDIR} || print_err_and_exit "Can't find build directory"
+
+if [ ! -d "${PWD}/CMakeFiles" ]; then
+    cmake .. || print_err_and_exit "CMake was unable to generate build files"
+fi
+
+make || print_err_and_exit "Unable to build project"
+
+echo ""
+
+./my_mp4_parser ../media/brandon.mp4
