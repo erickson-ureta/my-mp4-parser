09a8b33aed7f9eeea7edd8fb6355f5e0738f52c1
erickson-ureta <erick.ureta95@gmail.com>
2024-12-11 19:50:59 -0600

    Added list of box names in the constants header file.
    Also included the Python script used to generate the header file.

--- my_mp4_parser.cpp
+++ my_mp4_parser.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <algorithm>
 
 #include "constants.hpp"
 #include "log.hpp"
@@ -14,13 +15,14 @@ Mp4Parser::Mp4Parser(const std::string& fileName)
 bool
 Mp4Parser::parseMp4File()
 {
-    logger.info("Analyzing file: %s", _mFileName.c_str());
     if (!_loadFile(_mFileName))
     {
+        logger.error("Failed to open file: %s", _mFileName.c_str());
         return false;
     }
     if (!_isValidMp4File())
     {
+        logger.error("File is not a valid MP4 file: %s", _mFileName.c_str());
         return false;
     }
 
@@ -31,11 +33,7 @@ bool
 Mp4Parser::_loadFile(const std::string& fileName)
 {
     std::ifstream file(fileName, std::ios::binary);
-    if (!file)
-    {
-        logger.error("Failed to open file: %s", fileName.c_str());
-        return false;
-    }
+    if (!file) return false;
 
     file.seekg(0, std::ios::end);
     std::streamsize size = file.tellg();
@@ -43,34 +41,25 @@ Mp4Parser::_loadFile(const std::string& fileName)
 
     _mBuffer.resize(size);
 
-    if (!file.read(reinterpret_cast<char *>(_mBuffer.data()), size))
-    {
-        logger.error("Failed to open file: %s", fileName.c_str());
-        return false;
-    }
-
-    return true;
+    return static_cast<bool>(file.read(reinterpret_cast<char *>(_mBuffer.data()), size));
 }
 
 std::string
-Mp4Parser::_getAtomName(const AtomBufPtr &atom)
+Mp4Parser::_getAtomName(const AtomBufPtr &atomBuf)
 {
-    return BufferUtils::readBytesIntoStr(_mBuffer.cbegin()+ATOM_NAME_OFFSET,
+    return BufferUtils::readBytesIntoStr(atomBuf + ATOM_NAME_OFFSET,
                                          ATOM_NAME_LEN);
 }
 
 bool
 Mp4Parser::_isValidMp4File()
 {
-    if (!_mBuffer.size())
-    {
-        logger.error("File is empty: %s", _mFileName.c_str());
-        return false;
-    }
+    if (!_mBuffer.size()) return false;
 
     std::string firstAtomName = _getAtomName(_mBuffer.cbegin());
+    auto it = std::find(_knownAtomNames.begin(), _knownAtomNames.end(), firstAtomName);
 
-    return true;
+    return it != _knownAtomNames.end();
 }
 
 int
@@ -78,12 +67,16 @@ main(int argc, char **argv)
 {
     if (argc <= 1)
     {
-        logger.error("Not enough arguments.");
+        logger.error("No input files specified.");
+        logger.error("usage: ./%s <file1.mp4> <file2.mp4> ... <fileN.mp4>",
+                     argv[0]);
         return 1;
     }
 
     for (int i = 1; i < argc; i++)
     {
+        logger.info("----");
+        logger.info("Analyzing file: %s", argv[i]);
         Mp4Parser parser(argv[i]);
         parser.parseMp4File();
     }
--- my_mp4_parser.hpp
+++ my_mp4_parser.hpp
@@ -4,6 +4,8 @@
 #include <string>
 #include <vector>
 
+#include "constants.hpp"
+
 using AtomBufPtr = std::vector<uint8_t>::const_iterator;
 
 class Mp4Parser
@@ -20,6 +22,25 @@ class Mp4Parser
 
         // Helper methods
         bool _loadFile(const std::string& fileName);
-        std::string _getAtomName(const AtomBufPtr &atom);
+        std::string _getAtomName(const AtomBufPtr &atomBuf);
         bool _isValidMp4File();
+
+        // List of known atom names
+        const std::vector<std::string> _knownAtomNames = {
+            ATOM_FTYP, ATOM_PDIN, ATOM_MOOV, ATOM_MVHD, ATOM_META, ATOM_TRAK,
+            ATOM_TKHD, ATOM_TREF, ATOM_EDTS, ATOM_ELST, ATOM_MDIA, ATOM_MDHD,
+            ATOM_HDLR, ATOM_ELNG, ATOM_MINF, ATOM_VMHD, ATOM_SMHD, ATOM_HMHD,
+            ATOM_STHD, ATOM_NMHD, ATOM_DINF, ATOM_DREF, ATOM_STBL, ATOM_STSD,
+            ATOM_STTS, ATOM_CTTS, ATOM_CSLG, ATOM_STSC, ATOM_STSZ, ATOM_STS2,
+            ATOM_STCO, ATOM_CO64, ATOM_STSS, ATOM_STSH, ATOM_PADB, ATOM_STDP,
+            ATOM_SDTP, ATOM_SBGP, ATOM_SGPD, ATOM_SUBS, ATOM_SAIZ, ATOM_SAIO,
+            ATOM_UDTA, ATOM_MVEX, ATOM_MEHD, ATOM_TREX, ATOM_LEVA, ATOM_MOOF,
+            ATOM_MFHD, ATOM_TRAF, ATOM_THFD, ATOM_TRUN, ATOM_TFDT, ATOM_MFRA,
+            ATOM_TFRA, ATOM_MFRO, ATOM_MDAT, ATOM_FREE, ATOM_SKIP, ATOM_CPRT,
+            ATOM_TSEL, ATOM_STRK, ATOM_STRI, ATOM_STRD, ATOM_ILOC, ATOM_IPRO,
+            ATOM_SINF, ATOM_FRMA, ATOM_SCHM, ATOM_SCHI, ATOM_IINF, ATOM_XML,
+            ATOM_BXML, ATOM_PITM, ATOM_FIIN, ATOM_PAEN, ATOM_FIRE, ATOM_FPAR,
+            ATOM_FECR, ATOM_SEGR, ATOM_GITN, ATOM_IDAT, ATOM_IREF, ATOM_MECO,
+            ATOM_MERE, ATOM_STYP, ATOM_SIDX, ATOM_SSIX, ATOM_PRFT,
+        };
 };
