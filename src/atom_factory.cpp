30607a5a894e49b9bbb1aa28c4723857ebd56bf4
erickson-ureta <erick.ureta95@gmail.com>
2024-12-12 22:50:01 -0600

    Made the AtomFactory use a map of lambda functions to be populated with individual Atoms later on

--- src/atom_factory.cpp
+++ src/atom_factory.cpp
@@ -1,14 +1,25 @@
+#include <cstdint>
+#include <functional>
+#include <unordered_map>
+
 #include "atoms.hpp"
 #include "atom_factory.hpp"
 #include "constants.hpp"
 #include "logger.hpp"
 
+using AtomGenerator = std::function<std::shared_ptr<GenericAtom>(size_t, uint8_t*)>;
+
 std::shared_ptr<GenericAtom>
 AtomFactory::createAtom(const std::string &atomName, uint8_t *buf, size_t bufSize)
 {
-    if (atomName == ATOM_FTYP)
+    static const std::unordered_map<std::string, AtomGenerator> atomFactoryMap = {
+        {ATOM_FTYP, [](size_t size, uint8_t *rawBuffer){ return std::make_shared<FtypAtom>(size, rawBuffer); }}
+    };
+
+    auto it = atomFactoryMap.find(atomName);
+    if (it != atomFactoryMap.end())
     {
-        return std::make_shared<FtypAtom>(bufSize, buf);
+        return it->second(bufSize, buf);
     }
 
     return nullptr;
--- src/atoms.hpp
+++ src/atoms.hpp
@@ -90,4 +90,3 @@ class FtypAtom : public GenericAtom
 
         void _parseRawBufIntoFields() override;
 };
-
